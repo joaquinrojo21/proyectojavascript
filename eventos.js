e4413fe600d5b97ae944323b5b0dd072fb047572
Joaquin Rojo <rojo.joaquin88@gmail.com>
2022-12-27 14:04:33 -0300

    agregando funcionalidad al carrito

--- eventos.js
+++ eventos.js
@@ -4,8 +4,48 @@ let carrito = []
 
 const botonComprar = document.querySelector(".card-group");
 
-botonComprar.addEventListener("click", function() {
-carrito.push(botonComprar)
-console.log([carrito])
+botonComprar.forEach((card)=>{
+card.addEventListener("click", (e)=>{
+    leerDatosProducto(e.target.parentElement)
 })
+ })
+
+function leerDatosProducto(productos) {
+    //este es un objeto
+    const infoProducto ={
+        titulo: card-body.querySelector(".card-title").textContent,
+        texto: card-body.querySelector(".card-text").textContent,
+    }
+}
+
+//agregar elementos al carrito
+carrito=[...carrito, infoProducto ]
+
+//mostrar prodcutos en el carrito
+const mostrarCarrito = document.querySelector("#carrito")
+
+function carritoHtml(){
+    limpiarHTML();
+
+    articulosCarrito.forEach((producto) => {
+      const row = document.createElement("p");
+      row.innerHTML = `
+      <div class="container">
+      <h5>${producto.nombre}</h5>
+      <p>${producto.texto}</p>
+      <button class="btn btn-danger">Eliminar</button>
+      </div>
+      `;
+      carrito.appendChild(row);
+    });
+  }
+  
+  function limpiarHTML() {
+    carrito.innerHTML = "";
+  }
+
+//botonComprar.addEventListener("click", function() {
+//carrito.push(botonComprar)
+//console.log([carrito])
+//})
 
